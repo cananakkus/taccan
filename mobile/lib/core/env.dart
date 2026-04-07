a40cc2f893ddfceac23f6c26e472396adef260f2
Tacettin Sancak <ts.solidarity@gmail.com>
2026-04-07 21:16:01 +0300

    Fix Socket.IO path: /taccan/socket.io for Caddy reverse proxy

--- mobile/lib/core/env.dart
+++ mobile/lib/core/env.dart
@@ -1,5 +1,5 @@
 class Env {
   static const String baseUrl = 'https://play.wleeaf.dev';
-  static const String socketPath = '/socket.io';
+  static const String socketPath = '/taccan/socket.io';
   static const Duration ackTimeout = Duration(seconds: 7);
 }
